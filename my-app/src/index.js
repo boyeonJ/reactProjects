34ffe6295b8c15ef51a6da59ae8fa30a7a4a0e52
boyeon <qhdus96@naver.com>
2023-11-10 22:28:27 +0900

    add book list component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,37 @@
+import './index.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const Greeting = () => {
-    return <h2>My First Component</h2>
+const BookList = () => {
+    return (
+        <section className='booklist'>
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    );
+};
+const Book = () => {
+    return (
+        <article className='book'>
+            <Image />
+            <Title />
+            <Author />
+        </article>
+    );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Image = () => {
+    return <img src="https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL900_SR900,600_.jpg" alt="Fourth Wing"/>
+}
+const Title = () => {
+    return <h2>Fourth Wing</h2>
+}
+const Author = () => {
+    return <h4>Rebecca Yarros</h4>
+}
 
-root.render(<Greeting />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BookList />);
 
