a3c226c5fea8ef61e4534373955cebde615169e6
boyeon <qhdus96@naver.com>
2023-11-15 00:10:19 +0900

    add home

--- backroads/src/components/Home.js
+++ backroads/src/components/Home.js
@@ -1,6 +1,13 @@
 const Home = () => {
-    return <div class="home">Home</div>;
+    return (
+        <div class="home">
+            <h1>continue exploring</h1>        
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, recusandae.</p>
+            <a href="#" class="home-btn">explore tours</a>
+        </div>
+    );
 }
 
 
 export default Home;
+
